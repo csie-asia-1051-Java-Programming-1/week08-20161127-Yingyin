6b6889c0d12fc00f51621e3423216a137c69f806
Yingyin <105791120@live.asia.edu.tw>
2016-11-23 11:32:23 +0800

    hw01 105791120 杨钰莹

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,4 +1,7 @@
 package hw;
+
+import java.util.Scanner;
+
 /*
  * Topic: 請寫一個 遞迴函式)進行十進制數字轉換成二進制數字，例如十進制的 13 會輸出二進制的 1101
  * Date: 2016/11/21
@@ -7,8 +10,15 @@ package hw;
 public class hw01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sca=new Scanner(System.in);
+	int n=sca.nextInt();
+	System.out.println(tobinary(n));
+	}
 
+	public static String tobinary(int n) {
+		if(n>0)
+		return tobinary(n/2)+n%2;
+		return "";
 	}
 
 }
