129009dfbc6ea122efc1e6992cedcdfc90a2c476
Yingyin <105791120@live.asia.edu.tw>
2016-11-23 11:33:51 +0800

    hw03 105791120 杨钰莹

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,4 +1,7 @@
 package hw;
+
+import java.util.Scanner;
+
 /*
  * Topic: 請寫一個遞迴程式，輸入一個 int 型態的資料，並回傳該資料在二進制下有多少個位元是 1，例如: 輸入 13 得到結果為 3
  * Date: 2016/11/21
@@ -7,8 +10,14 @@ package hw;
 public class hw03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca = new Scanner(System.in);
+    int n=sca.nextInt();
+    System.out.println(sum(n));
 	}
-
+   public static int sum(int n)
+   {
+	   if(n>0)
+		   return sum(n/2)+n%2;
+	   return 0;
+   }
 }
