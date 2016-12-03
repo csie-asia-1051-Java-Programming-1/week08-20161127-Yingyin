a83f00ddd051d0a19dfef0b6cbaaea9ed78de77a
Yingyin <105791120@live.asia.edu.tw>
2016-12-03 11:25:33 +0800

    ex04 105791120 杨钰莹

--- src/ex/ex04.java
+++ src/ex/ex04.java
@@ -1,4 +1,7 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 請用類別撰寫一個計算機，透過計算機物件可以計算 加、減、乘、除、平方、立方、開根號的功能
  * Date: 2016/11/21
@@ -8,8 +11,46 @@
 public class ex04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Scanner sca=new Scanner(System.in);
+		computer cop = new computer();
+		float a=sca.nextFloat();
+		char b=sca.next().charAt(0);
+		float c=0;
+		if(b=='+' || b=='*' || b=='/'){
+			c=sca.nextInt();
+			}
+		System.out.println(computer.fun1(a,b,c));
+	}		
 
 }
+
+  class computer{
+	  
+	  public static float fun1(float a,char b,float c){
+		  float x=0;
+		  switch(b){
+		  case '+':
+			  x=a+c;
+			  break;
+		  case '-':
+			  x=a-c;
+			  break;
+		  case '*':
+			  x=a*c;
+			  break;
+		  case '/':
+			  x=a/c;
+			  break;
+		  case '^':
+			  x=(float)Math.pow(a, 2);
+			  break;
+		  case '3':
+			  x=(float)Math.pow(a, 3);
+			  break;
+		  case 'L':
+			  x=(float)Math.sqrt(a);
+			  break;
+		  }
+		return x;
+	  }
+  }
